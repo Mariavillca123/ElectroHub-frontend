f79fcbbf29ecb36501799bfb98769fdf2fef75eb
Mariavillca123 <mariabelenvillcacevallos@gmail.com>
2026-01-29 01:56:35 -0500

    Fix: Add JWT token interceptor to axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,4 +4,18 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Interceptor para agregar el token automáticamente a todas las peticiones
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
