92f7fff29d88ee5cc652b19813e77c81d46913da
Mariavillca123 <mariabelenvillcacevallos@gmail.com>
2026-02-19 23:34:59 -0500

    Remove extra closign divs

--- src/pages/Public/CartPage.tsx
+++ src/pages/Public/CartPage.tsx
@@ -160,7 +160,6 @@ export default function CartPage() {
                         </button>
                       </div>
                     </div>
-                  </div>
                   ))}
                 </div>
               </div>
